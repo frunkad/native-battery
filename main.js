6f84291d3d2a429618d437039652fd73d572a085
Darshan Baid <darshanbaid22@gmail.com>
2018-07-29 21:39:58 +0530

    skelleton

---
+++ main.js
@@ -0,0 +1,37 @@
+const {app, BrowserWindow} = require('electron')
+  
+  // Keep a global reference of the window object, if you don't, the window will
+  // be closed automatically when the JavaScript object is garbage collected.
+  let win
+  
+  function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({width: 800, height: 700})
+      win.loadFile('index.html')
+    // win.webContents.openDevTools()
+      win.on('closed', () => {
+      win = null
+    })
+  }
+  
+  app.on('ready', createWindow)
+  
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow()
+    }
+  })
+  
+  // In this file you can include the rest of your app's specific main process
+  // code. You can also put them in separate files and require them here.
